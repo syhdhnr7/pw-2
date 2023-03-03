c8773fe887f6b11d74f2f253ca76335014753d4f
Nur Syahidah <114471577+syhdhnr7@users.noreply.github.com>
2023-03-03 14:18:19 +0700

    Add files via upload

---
+++ pertemuan_1/array_assosiatif.php
@@ -0,0 +1,11 @@
+<?php
+// array assosiatif
+$student = [
+    'name' => 'Nur Syahidah',
+    'nim' => 11222273,
+    'major' => 'Informatics'
+];
+
+// mengakses array assosiatif
+echo "Jurusan saya adalah $student[major]";
+?>
---
+++ pertemuan_1/tipe_data.php
@@ -0,0 +1,17 @@
+<?php
+
+// string
+$nama = "Nur Syahidah";
+
+// integer
+$umur = "18";
+
+// float
+$ipk = 3.9;
+
+// boolean
+$isStudent = true;
+
+// menampilkan tipe data dari sebuah varible
+var_dump($nama);
+?>
