77b924c22af8b6d675c5d2d5411d2abe342634ba
Nur Syahidah <114471577+syhdhnr7@users.noreply.github.com>
2023-03-03 11:17:39 +0700

    Add files via upload

---
+++ pemweb01/array.php
@@ -0,0 +1,12 @@
+<?php
+// array indexing
+$animals = ['Kambing', 'Kucing', 'Sapi'];
+
+// mengakses array indexing
+// echo $animals[0];
+// echo "<br>" . $animals[1];
+// echo "<br>" . $animals[2];
+foreach($animals as $animal){
+    echo $animal . '<br>';
+}
+?>
